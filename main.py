6ea4f27d45be43a8a06f5681f53739558be52deb
بدر إسماعيل أشكناني <baderash2006@gmail.com>
2023-09-06 19:50:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
 # write your code here
+person = {
+    "name": "swiper",
+    "age": "17",
+    "hobbies": ["swiping"]
+
+}
+print (person["name"])
+print (person["age"])
+
+person["age"] = "18"
+person["country"] = "Kuwait"
+print (person)
+
+person["hobbies"].append("repenting")
+
+def check_hobbies(person):
+    if len(person["hobbies"])>=2:
+        print(f'wow you are astonishing')
+
+check_hobbies(person)
+
+
